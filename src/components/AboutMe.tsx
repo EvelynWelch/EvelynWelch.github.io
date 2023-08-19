cd42528788c02bcacafa9dbbe78568b90d133cb4
Evelyn <evelynwelch.dev@gmail.com>
2023-08-19 13:06:50 -0600

    moved the header links to AboutMe and added an 'about me' that will display AboutMe when clicked.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import { toggleElementVisibility, HIDDEN_CLASS_NAME } from "../utilities"
 
 export const ABOUT_ME_ID = "about-me"
 
 interface Props {
     aboutMe: string,
+    email: string
+    github: string
+    githubPersonal: string
+    linkedin: string
 }
 
 export function toggleAboutMeVisibility() {
@@ -33,12 +37,50 @@ export function getAboutMe() {
 }
 
 function AboutMe(props: Props): JSX.Element {
+    const [isExpanded, setIsExpanded] = useState(true)
+    // const [triggerRender, setTriggerRender] = useState(1)
+
+    // let isExpanded: boolean
+    // const about = getAboutMe()
+    // if (about?.classList.contains(HIDDEN_CLASS_NAME) && isExpanded != true) {
+    //     // isExpanded = true
+    //     setIsExpanded(true)
+    
+    // } else if(!about?.classList.contains(HIDDEN_CLASS_NAME) && isExpanded != false)
+    // {
+    //     setIsExpanded(false)
+    //     // setIsExpanded(false)
+        
+    // }
     return (
-        <div className="aboutMe text-wrapper" id={ABOUT_ME_ID}>
-            <article>
-                <h2>About me:</h2>
-                <p>{props.aboutMe}</p>
-            </article>
+        <div>
+            <div className="contact-info">
+                <div id="aboutMe-button" className="aboutMe-button" onClick={() => {
+                    if(isExpanded) {
+                        setIsExpanded(false)
+                        hideAboutMe()
+                        
+                    } else {
+                        setIsExpanded(true)
+                        showAboutMe()
+                        
+                    }
+                }}>
+                    {/* {isExpanded ? '\u25B2' : '\u25BC'} */}
+                    <span>About me {isExpanded ? '\u25B2' : '\u25BC'}</span>
+                </div>
+                <span>{props.email}</span>
+                <span><a href={props.github}>github</a> </span>
+                <span><a href={props.githubPersonal}>personal github</a> </span>
+                <span><a href={props.linkedin}>LinkedIn</a> </span>
+            </div>
+            <div className="aboutMe text-wrapper" id={ABOUT_ME_ID}>
+
+                <article>
+                    <h2>About me:</h2>
+                    <p>{props.aboutMe}</p>
+                </article>
+            </div>
         </div>
     )
 }

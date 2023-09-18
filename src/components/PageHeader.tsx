9625f1ac02b0b200bbcc66fab7d110a2f6e96e10
Evelyn <evelynwelch.dev@gmail.com>
2023-09-18 13:50:58 -0600

    Changed ProjectCard to a generic tabbed view NOTE: it requires specific styleing to work. Added the About Me  as a tab instead of a separate component.

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -13,6 +13,13 @@ function PageHeader(props: Props): JSX.Element {
         <div className="pageHeader">
             <header>
                 <h1>{props.title} - <span>{props.jobTitle}</span></h1>
+                <div className="links">
+                    {/* TODO: figure out how to make this justify to left but not go further than the title */}
+                    <span>{props.email}</span>
+                    <span><a href={props.github}>github</a> </span>
+                    <span><a href={props.githubPersonal}>personal github</a> </span>
+                    <span><a href={props.linkedin}>LinkedIn</a> </span>
+                </div>
             </header>
         </div>
     )
---
+++ src/components/TabbedView.tsx
@@ -0,0 +1,158 @@
+// A simple tabbed view react component.
+
+import { useState } from 'react';
+
+// create a random string of length n for unique id's
+function makeid(length: number): string {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+
+interface TabProps {
+    title: string
+    thumbnailSrc?: string
+    thumbnailAlt?: string
+    key: string
+}
+
+function Tab(props: TabProps, onclick: Function): JSX.Element {
+    // The onclick will toggle the display of TabBody
+    return (
+        <div className="thumbnail" id={props.key} key={props.key} onClick={e => onclick(e)}>
+            <img src={props.thumbnailSrc} alt={props.thumbnailAlt} />
+            <h2>{props.title}</h2>
+        </div>
+    )
+}
+
+
+interface TabBodyProps {
+    title: string
+    description: string
+    about: string
+    key: string
+    link?: string
+    thumbnailSrc?: string
+    thumbnailAlt?: string
+    technologies?: string
+    
+}
+
+function TabBody(props: TabBodyProps, onclick: Function): JSX.Element {
+    return (
+        <div className="projectCard">
+            <div className='projectCard-X' onClick={e => onclick(e)}>
+                <button name="close-focused-project">X</button>
+            </div>
+            <article>
+                <div className="projectCard-text-wrapper">
+                    {props.link != null &&
+                        <h2><a href={props.link}>{props.title} </a></h2>
+                    }
+                    <h3>{props.description}</h3>
+                    {props.technologies != null &&
+                        <p>{props.technologies}</p>
+                    }
+                    <p>{props.about}</p>
+                </div>
+                {props.thumbnailSrc != null &&
+                    <div className="img-wrapper">
+                        <img src={props.thumbnailSrc} alt={props.thumbnailAlt} />
+                    </div>
+                }
+            </article>
+        </div>
+    )
+}
+
+
+function getTabProps(tabBodyProps: TabBodyProps): TabProps {
+    const tabProps = {
+        title: tabBodyProps.title,
+        key: tabBodyProps.key,
+        thumbnailSrc: tabBodyProps.thumbnailSrc,
+        thumbnailAlt: tabBodyProps.thumbnailAlt
+    }
+    return tabProps
+}
+
+function tabFactory(tabData: TabBodyProps[], onclick: Function): JSX.Element[] {
+    // onclick's deals with TabView's state, so the function must be defined there and passed down
+    let tabs: JSX.Element[] = []
+    tabData.forEach(e => {
+        // strip unused data
+        let tabData = getTabProps(e)
+        tabs.push(Tab(tabData, onclick))
+    })
+    return tabs
+}
+
+export function TabView(props: TabBodyProps[]): JSX.Element {
+    // TODO: make the default the AboutMe?
+    const [focusedTab, setfocusedTab] = useState<JSX.Element | null>(null)
+
+    const tabData = props
+    let focusedTabKey: string | null = null
+
+    let tabs: JSX.Element[] = tabFactory(
+        tabData,
+
+        (e: any) => {
+            // get the clicked element, and its id
+            let clickedElement = e.currentTarget
+            let clickedId = clickedElement.id
+            focusedTabKey = clickedId
+
+            // add the selected-project class
+            clickedElement.classList.add("selected-project")
+
+            // make sure only one Tab is the "selected-project"
+            tabData.forEach(element => {
+                if (element.key != clickedId) {
+                    let elem = document.getElementById(element.key)
+                    if (elem!.classList.contains("selected-project")) {
+                        elem!.classList.remove("selected-project")
+                    }
+                }
+            })
+
+            // find that id in tabs array and set it as the focused project 
+            for (let i = 0; i < tabData.length; i++) {
+                if (tabData[i].key == clickedId) {
+                    let data = { ...tabData[i] }
+                    data.key = makeid(5)
+                    // make the 'x' close the focused project
+                    setfocusedTab(TabBody(
+                        data,
+                        () => {
+                            setfocusedTab(null)
+                            focusedTabKey = null
+                        }
+                    ))
+                    break;
+                }
+            }
+        }
+    )
+
+    return (
+        <div className='tabs-container'>
+            <div className="thumbnail-container">
+                {tabs}
+            </div>
+            <div className="">
+                {focusedTab}
+            </div>
+        </div>
+    )
+
+}
+
+

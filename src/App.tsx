e8caab1f3e533a0734f5526fd9ccb30dd4588055
Evelyn <evelynwelch.dev@gmail.com>
2023-09-18 13:53:07 -0600

    was in the wrong dir so not all changes went through again...

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 import PageHeader from "./components/PageHeader";
-import AboutMe from "./components/AboutMe";
-import { Projects} from './components/ProjectCard';
+import { TabView } from './components/TabbedView';
 import { makeid } from './utilities';
 
 // TODO: figure out how to make it center without adding the "height: 1000px to the css"
 
-
 const data = require("./portfolioData");
 
 const projectCardData = [
+  data.aboutMeTab,
   data.projectStatMiner,
   data.projectBraveNW,
   data.projectMesh,
   data.projectBraveNWdb
 ]
-// create uniqe id's for projects
+
 projectCardData.forEach(element => element.key = makeid(5))
 
-// const thumbnailData = getThumbnailProps(projectCardData)
-// thumbnailData.push({
-//   title: "About Me",
-//   imgSrc: "",
-//   imgAlt: "",
-//   key: "1234"
-// })
-
-// const pc =  <ProjectCard {...data.projectStatMiner}/>
-// const projectCards = projectCardFactory(projectCardData)
 function App() {
   return (
    
     <div className="App">
       
         <PageHeader {...data.pageHeader}/> 
-        <AboutMe {...data.aboutMe}/>
+
 
   
-        {Projects(projectCardData)}
+        {TabView(projectCardData)}
 
     
     </div>
